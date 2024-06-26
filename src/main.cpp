1f92722bb1790ef1f030eba08cdea3c9997cde2e
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2024-06-27 00:37:35 +0800

    refactor: Change tabs to spaces

--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,50 @@ void entryRoutine(byte *cardUID);
 
 void setup()
 {
-	module.init();
+    module.init();
 }
 
 void loop()
 {
-	bool keyInserted = digitalRead(Module::KEY_PIN);
-	digitalWrite(Module::Y_LED_PIN, keyInserted ? HIGH : LOW); // turn on yellow LED if key is inserted
+    bool keyInserted = digitalRead(Module::KEY_PIN);
+    digitalWrite(Module::Y_LED_PIN, keyInserted ? HIGH : LOW); // turn on yellow LED if key is inserted
 
-	byte *cardUID;
-	bool readStatus = module.readCardUID(cardUID);
-	if (!readStatus)
-		return;
+    byte *cardUID;
+    bool readStatus = module.readCardUID(cardUID);
+    if (!readStatus)
+        return;
 
-	if (keyInserted)
-	{
-		// try to write the card UID to EEPROM
-		if (module.writeAccessRecord(cardUID))
-			digitalWrite(Module::G_LED_PIN, HIGH); // turn on green LED if successful
-		else
-			digitalWrite(Module::R_LED_PIN, HIGH); // turn on red LED if failed
+    if (keyInserted)
+    {
+        // try to write the card UID to EEPROM
+        if (module.writeAccessRecord(cardUID))
+            digitalWrite(Module::G_LED_PIN, HIGH); // turn on green LED if successful
+        else
+            digitalWrite(Module::R_LED_PIN, HIGH); // turn on red LED if failed
 
-		delay(1000);
-		digitalWrite(Module::G_LED_PIN, LOW);
-		digitalWrite(Module::R_LED_PIN, LOW);
-	}
-	else
-	{
-		entryRoutine(cardUID);
-	}
+        delay(1000);
+        digitalWrite(Module::G_LED_PIN, LOW);
+        digitalWrite(Module::R_LED_PIN, LOW);
+    }
+    else
+    {
+        entryRoutine(cardUID);
+    }
 }
 
 void entryRoutine(byte *cardUID)
 {
-	// check if the detected card is in the access record
-	if (module.checkAccess(cardUID))
-	{
-		module.engageLock();
-		delay(1000);
-		module.disengageLock();
-	}
-	else
-	{
-		digitalWrite(Module::R_LED_PIN, HIGH);
-		delay(1000);
-		digitalWrite(Module::R_LED_PIN, LOW);
-	}
+    // check if the detected card is in the access record
+    if (module.checkAccess(cardUID))
+    {
+        module.engageLock();
+        delay(1000);
+        module.disengageLock();
+    }
+    else
+    {
+        digitalWrite(Module::R_LED_PIN, HIGH);
+        delay(1000);
+        digitalWrite(Module::R_LED_PIN, LOW);
+    }
 }
